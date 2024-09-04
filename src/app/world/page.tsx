1b8994b0417c743294d6f0b5b0b2ae8f6db7085a
Guilherme Vieira <hi@guilhermevieira.dev>
2024-09-04 20:55:48 -0300

    fix(build): add suspense to /world page

--- src/app/world/page.tsx
+++ src/app/world/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { ExperienceByWorldTable } from "@/components/experience-by-world/experience-by-world-table";
+import { Suspense } from "react";
 
 export default function World() {
-  return <ExperienceByWorldTable />;
+  return (
+    <Suspense>
+      <ExperienceByWorldTable />;
+    </Suspense>
+  );
 }
