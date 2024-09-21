83468b95de1ef02bbd403d3fc55be2cedf7f8c64
Guilherme Vieira <hi@guilhermevieira.dev>
2024-09-21 17:44:35 -0300

    feat(hotjar): add hotjar

--- src/components/utils/hotjar.tsx
+++ src/components/utils/hotjar.tsx
@@ -1,7 +1,22 @@
 "use client";
-import Hotjar from "@hotjar/browser";
+import Script from "next/script";
 export const HotjarSnippet = () => {
-  const siteId = process.env.NEXT_PUBLIC_HOTJAR_SITE_ID;
-  const hotjarVersion = 6;
-  return Hotjar.init(Number(siteId), hotjarVersion);
+  return (
+    <Script
+      strategy="afterInteractive"
+      id="hotjar"
+      dangerouslySetInnerHTML={{
+        __html: `
+        (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_SITE_ID},hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `,
+      }}
+    />
+  );
 };
