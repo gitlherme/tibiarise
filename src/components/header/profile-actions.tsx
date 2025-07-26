9bae4d83d0bbff73ec88538ff7257e759b7e92c2
Guilherme Vieira <code@gitlher.me>
2025-07-25 22:46:56 -0300

    fix: add go to dashboard button

--- src/components/header/profile-actions.tsx
+++ src/components/header/profile-actions.tsx
@@ -1,37 +1,12 @@
 "use client";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarImage } from "../ui/avatar";
-import { AvatarFallback } from "@radix-ui/react-avatar";
-import { useSession } from "next-auth/react";
 import { Link } from "@/i18n/routing";
+import { Button } from "../ui/button";
 
 export const ProfileActions = () => {
-  const { data: session } = useSession();
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src={session?.user?.image || ""} />
-          <AvatarFallback>
-            {session?.user?.name?.charAt(0) || "U"}
-          </AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Link href="/dashboard/characters">Dashboard</Link>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Link href="/dashboard/characters">
+      <Button>Go to Dashboard</Button>
+    </Link>
   );
 };
