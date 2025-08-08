949a63b7bfd7ab6f79da2c8579859842837ef3da
Guilherme Vieira <code@gitlher.me>
2025-08-08 18:38:21 -0300

    fix: add name in support

--- src/app/[locale]/(public)/support/page.tsx
+++ src/app/[locale]/(public)/support/page.tsx
@@ -2,21 +2,20 @@
 
 import { useTranslations } from "next-intl";
 import Link from "next/link";
-import { getCookie } from "cookies-next/client";
 
 export default function About() {
   const t = useTranslations("SupportPage");
-  const locale = getCookie("NEXT_LOCALE") || "en";
   return (
     <div className="text-center flex flex-col gap-8">
-      <h1 className="font-black text-3xl text-blue-500">{t("title")}</h1>
+      <h1 className="font-black text-3xl text-primary">{t("title")}</h1>
       <div className="flex flex-col gap-4">
         <p>
           {t.rich("message1", {
             character: (chunks) => (
               <Link
-                className="text-blue-500 font-bold"
-                href={`/${locale}/character/Gui`}
+                className="text-primary font-bold"
+                href={`https://www.tibia.com/community/?name=Rise+Donations`}
+                target="_blank"
               >
                 {chunks}
               </Link>
@@ -28,7 +27,7 @@ export default function About() {
           {t.rich("message3", {
             discord: (chunks) => (
               <Link
-                className="text-blue-500 font-bold"
+                className="text-primary font-bold"
                 href={`https://discord.gg/BAZDE29Eyf`}
               >
                 {chunks}
